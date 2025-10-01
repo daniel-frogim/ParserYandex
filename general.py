c5c00084cd73383078eb41c4feab5a113a0cfdb2
daniel-frogim <danielfrogim@gmail.com>
2025-10-01 19:48:14 +0300

    Add files via upload

---
+++ general.py
@@ -0,0 +1,67 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import json
+import openpyxl
+
+
+with open('input.json', 'r', encoding='utf-8') as file:
+    input_data = json.load(file)
+
+
+city = input_data['Geoposition']
+city = city.replace(" ", "%20")
+Mtype = input_data['Type']
+
+url = "https://yandex.ru/maps/1/moscow-and-moscow-oblast/search/"
+url += city + "%20" + Mtype
+driver = webdriver.Chrome()
+
+driver.get(url)
+
+check = input("Введи Enter когда долистаешь")
+
+
+html = driver.page_source
+bs = BeautifulSoup(html,"lxml")
+
+
+ul_li = bs.find_all('div', 'search-business-snippet-view__content')
+
+data = {
+    "name": [],
+    "rating": [],
+    "href": []
+    }
+for i in range(len(ul_li)):
+    EAX = ul_li[i].find('div', 'search-business-snippet-view__title')
+    EDX = ul_li[i].find('a', 'search-business-snippet-view__rating')
+    if EAX is not None and EDX is not None:
+        data['name'].append(EAX.text)
+        data['rating'].append(EDX.text)
+        data['href'].append('https://yandex.ru' + EDX.get('href'))
+
+
+with open('output.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
+count = len(ul_li)
+if len(data['rating']) < count:
+    count = len(data['rating'])
+
+
+for i in range(count):
+    rating_split = data['rating'][i].split(" ")
+    sheet['A' + str(i + 1)] = data['name'][i]
+    sheet['B' + str(i + 1)] = city.replace('%20', ' ')
+    sheet['C' + str(i + 1)] = rating_split[0]
+    sheet['D' + str(i + 1)] = rating_split[1]
+    sheet['E' + str(i + 1)] = data['href'][i]
+
+
+workbook.save("Общее/" + city.replace('%20', ' ') + " " + Mtype + ".xlsx")
+
+print("\n\nВСЕ СДЕЛАНО\n\n")
---
+++ reviews.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import json
+import openpyxl
+
+
+with open('input.json', 'r', encoding='utf-8') as file:
+    input_data = json.load(file)
+
+
+url = input_data['reviews']
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+check = input("Введи Enter когда долистаешь")
+
+
+html = driver.page_source
+bs = BeautifulSoup(html,"lxml")
+
+Name = bs.find('h1', 'orgpage-header-view__header')
+reviews = bs.find_all('div', 'business-reviews-card-view__review')
+
+data = {
+    "author": [],
+    "text": [],
+    "date": [],
+    "rating": []
+    }
+
+
+for i in range(len(reviews)):
+    author = reviews[i].find('div', 'business-review-view__author-name')
+    text = reviews[i].find('span', 'business-review-view__body-text')
+    date = reviews[i].find('span', 'business-review-view__date')
+    rating = reviews[i].find('meta', itemprop='ratingValue')
+    data['author'].append(author.text)
+    data['text'].append(text.text)
+    data['date'].append(date.text)
+    data['rating'].append(rating.get('content'))
+
+
+with open('output_reviews.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+
+for i in range(len(reviews)):
+    rating_split = data['rating'][i].split(" ")
+    sheet['A' + str(i + 1)] = data['author'][i]
+    sheet['B' + str(i + 1)] = data['rating'][i]
+    sheet['C' + str(i + 1)] = data['date'][i]
+    sheet['D' + str(i + 1)] = data['text'][i]
+
+workbook.save("Отзывы/" + Name.text + " reviews.xlsx")
+
+
+print("\n\nВСЕ СДЕЛАНО\n\n")
